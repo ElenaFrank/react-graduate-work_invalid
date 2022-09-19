bda5a9bc6ca6d37c61e5ca38730b52ff3af62823
Alyona <alyonazholob3@gmail.com>
2022-09-19 20:48:56 +0700

    add module 'pagination.jsx'

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,12 @@ import React, {useState} from "react";
 import Users from "../src/components/users";
 import Status from "../src/components/searchStatus";
 import API from "./API"
+import Pagination from "./components/pagination";
 
 function App() {
     const [users, setUsers] = useState(API.users.fetchAll())
+    const count = users.length
+    const pageSize = 4
 
     const handleDeleteRow = (id) => {
         setUsers(users.filter(user => user._id !== id) )
@@ -18,33 +21,41 @@ function App() {
         setUsers(newUsers)
     }
 
+    const handlePageChange = (param) => {
+        
+
+    }
+     
+
     return (
         <>
-            <Status length = {users.length}></Status>
-            {users.length !==0 && 
-                                <table className="table">
-                                <thead>
-                                <tr>
-                                    <th scope="col">Имя</th>
-                                    <th scope="col">Качества</th>
-                                    <th scope="col">Провфессия</th>
-                                    <th scope="col">Встретился, раз</th>
-                                    <th scope="col">Оценка</th>
-                                    <th scope="col">Избранное</th>
-                                    <th scope="col"></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    <Users
-                                        usersList = {users}
-                                        onDeleteRow = {handleDeleteRow}
-                                        onBookMark = {handleTogBookmark}
-                                    >
+            <Status length = {count}></Status>
+            {count !==0 && 
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Провфессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <Users
+                            usersList = {users}
+                            onDeleteRow = {handleDeleteRow}
+                            onBookMark = {handleTogBookmark}
+                        >
 
-                                    </Users>
-                                </tbody>
-                            </table>
+                        </Users>
+                    </tbody>
+                </table>
+                
             }
+            <Pagination count={count} pageSize={pageSize} onPageChange={handlePageChange}></Pagination>
         </>
     )
 }
