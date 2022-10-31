14c8e1333c11eb94224d930f8ded764c86b840df
Жолоб Алена Владимировна <a.zholob@khakasia.local>
2022-10-31 08:11:05 +0700

    test

--- src/json/getUsers.js
+++ src/json/getUsers.js
@@ -1,11 +1,20 @@
-const getUsers = () => {
+import { useState } from "react"
+export function getUsers() {
+    const [users, setUsers] = useState([])
     fetch("http://localhost:3001/user", { method: "GET" })
-        .then((response) => {
+        .then(response => {
+            console.log(response)
             return response.json()
         })
-        .then((data) => {
-            return data
+        .then(data => {
+            console.log(data)
+            setUsers(data)
+            return users
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        .finally(() => {
+            console.log("Get!")
         })
 }
-
-export default getUsers
--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,21 +1,36 @@
-import React from "react"
-import UsersTest from "../json/testUsers"
-import getUsers from "../json/getUsers"
+import React, { useState } from "react"
+// import { getUsers } from "../json/getUsers"
 
 const Login = () => {
-    const users = getUsers()
-    console.log(users)
-    if (users) {
-        return (
-            <>
-                console.log(users)
-                {users.map(user => {
-                    // eslint-disable-next-line react/jsx-key
-                    return <UsersTest id={user.id} name = {user.name} />
-                })}
-            </>
-        )
-    } else return "Loading ..."
+    const [users, setUsers] = useState([])
+    fetch("http://localhost:3001/user", { method: "GET" })
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            setUsers(data)
+            return (
+                <>
+                    {users.map(user => {
+                        return <span key={user.id}>{user.name}</span>
+                    })}
+                </>
+            )
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        .finally(() => {
+            console.log("Get!")
+        })
+    // return (
+    //     <>
+    //         {users.map(user => (
+    //             <span key={user.id}>{user.name}</span>
+    //         ))}
+    //     </>
+    // )
 }
 
 export default Login
