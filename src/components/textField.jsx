2b4767772312c2e42e34d55810da0b8b47fa5d32
ElenaFrank <alyonazholob3@gmail.com>
2022-11-06 19:20:35 +0700

    add showing password

--- src/components/textField.jsx
+++ src/components/textField.jsx
@@ -1,24 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import PropType from "prop-types"
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+    const [showPassword, setShowPassword] = useState(false)
+    const getInputClasses = () => {
+        return "form-control" + (error ? " is-invalid" : "")
+    }
+    const toggleShowPassword = () => {
+        setShowPassword((prevstate) => !prevstate)
+    }
+
     return (
         <>
             <div className="mb-4">
-                <label
-                    htmlFor={name}
-                >
-                    {label}
-                </label>
-                <input
-                    type={type}
-                    id={name}
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                />
+                <label htmlFor={name}>{label}</label>
+                <div className="input-group">
+                    <input
+                        type={showPassword ? "text" : type}
+                        id={name}
+                        name={name}
+                        value={value}
+                        onChange={onChange}
+                        className={getInputClasses()}
+                    />
+                    {type === "password" && <button
+                        className="btn btn-outline-secondary"
+                        type="button"
+                        onClick={toggleShowPassword}
+                    >
+                        <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+                    </button>
+                    }
+                </div>
                 {error &&
-                <div className="invalid-feedback">
+                <div className="invalid-feedback" style={{ display: "contents" }}>
                     {error}
                 </div>}
             </div>
