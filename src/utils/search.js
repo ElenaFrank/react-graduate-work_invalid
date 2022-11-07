8d5f8553f9e83d87ff328249dca1b48bfbec33df
Жолоб Алена Владимировна <a.zholob@khakasia.local>
2022-11-07 08:52:51 +0700

    test

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,7 +1,6 @@
 export function searchData(data, value) {
-    // const dataUser = selectedProf ? filteredUsers : allUsers
     const found = data.filter(dataEl => {
-        return dataEl.name.toLowerCase().includes(value)
+        return dataEl?.name.toLowerCase().includes(value)
     })
     return found
 }
